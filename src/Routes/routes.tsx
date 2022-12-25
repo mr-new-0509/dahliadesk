803875633d17f266d136aeeec719cd16705b7652
mr-new-0509 <mr.new0509@gmail.com>
2022-12-25 15:22:53 +0900

    first commit

---
+++ src/Routes/routes.tsx
@@ -0,0 +1,23 @@
+import { Navigate } from "react-router-dom";
+import Orders from "../pages/Orders";
+import WaitingList from "../pages/WaitingList";
+import { IRoute } from "../utils/interfaces";
+
+export const routes: Array<IRoute> = [
+  {
+    name: 'Waiting Lists',
+    icon: 'fluent:document-bullet-list-clock-24-filled',
+    path: '/waiting-list',
+    element: <WaitingList />
+  },
+  {
+    name: 'Orders',
+    icon: 'fluent:clipboard-task-list-ltr-24-filled',
+    path: '/orders',
+    element: <Orders />
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" />
+  }
+]
