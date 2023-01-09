c8719d57fcf79dddbbc2941ce4fc22630fe8de3d
mr-new-0509 <mr.new0509@gmail.com>
2023-01-09 12:00:40 +0900

    select Accounts fix

--- src/components/DialogConnectWallet.js
+++ src/components/DialogConnectWallet.js
@@ -1,6 +1,6 @@
 /* global AlgoSigner */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon as MuiIcon, Dialog, DialogContent, IconButton, Stack, Typography, List, ListItem, ListItemIcon, Avatar, ListItemAvatar, ListItemText, ListItemButton } from '@mui/material';
 import { Icon } from '@iconify/react';
 import { grey } from '@mui/material/colors';
@@ -10,12 +10,16 @@ import useAlertMessage from '../hooks/useAlertMessage';
 import { MSG_NO_ACCOUNT, MSG_NO_ALGO_SIGNER, WALLET_ALGO_SIGNER, WALLET_MY_ALGO, WALLET_PERA, WARNING } from '../utils/constants';
 import useConnectWallet from '../hooks/useConnectWallet';
 import { getBalanceOfCurrentUser } from '../utils/functions';
+import DialogSelectWalletAccount from './DialogSelectWalletAccount';
 
 const myAlgoWallet = new MyAlgoConnect();
 
 export default function DialogConnectWallet({ dialogOpened, setDialogOpened, network }) {
   const { openAlert } = useAlertMessage();
   const { connectAct, disconnectAct } = useConnectWallet();
+
+  const [accounts, setAccounts] = useState([]);
+  const [dialogSelectAccountOpened, setDialogSelectAccountOpened] = useState(false);
 
   const closeDialog = () => {
     setDialogOpened(false);
@@ -27,11 +31,16 @@ export default function DialogConnectWallet({ dialogOpened, setDialogOpened, net
   const connectByAlgoSigner = async () => {
     if (typeof AlgoSigner !== 'undefined') {
       await AlgoSigner.connect();
-      let accounts = await AlgoSigner.accounts({
+      let walletAccounts = await AlgoSigner.accounts({
         ledger: network
       });
-      if (accounts.length > 0) {
-        connectAct(network, accounts[0].address, WALLET_ALGO_SIGNER);
+      setAccounts(walletAccounts);
+      if (walletAccounts.length > 0) {
+        if (walletAccounts.length > 1) {
+          setDialogSelectAccountOpened(true);
+        } else {
+          connectAct(network, walletAccounts[0].address, WALLET_ALGO_SIGNER);
+        }
         closeDialog();
       } else {
         openAlert({
@@ -51,11 +60,12 @@ export default function DialogConnectWallet({ dialogOpened, setDialogOpened, net
    * Connect wallet by MyAlgo wallet
    */
   const connectByMyAlgo = async () => {
-    let accounts = await myAlgoWallet.connect();
-    if (accounts.length > 0) {
+    let walletAccounts = await myAlgoWallet.connect();
+    setAccounts(walletAccounts);
+    if (walletAccounts.length > 0) {
       console.log('>>>>>>>> myAlgoWallet => ', myAlgoWallet);
-      connectAct(network, accounts[0].address, WALLET_MY_ALGO, myAlgoWallet);
-      getBalanceOfCurrentUser(accounts[0].address, network);
+      connectAct(network, walletAccounts[0].address, WALLET_MY_ALGO, myAlgoWallet);
+      getBalanceOfCurrentUser(walletAccounts[0].address, network);
       closeDialog();
     } else {
       openAlert({
@@ -74,92 +84,100 @@ export default function DialogConnectWallet({ dialogOpened, setDialogOpened, net
     });
 
     peraWallet.connect()
-      .then(accounts => {
+      .then(walletAccounts => {
+        setAccounts(walletAccounts);
         // Setup the disconnect event listener
         peraWallet.connector?.on("disconnect", () => {
           peraWallet.disconnect();
           disconnectAct();
         });
-        console.log('>>>>>>>> accounts => ', accounts);
-        connectAct(network, accounts[0].address, WALLET_PERA);
+        console.log('>>>>>>>> walletAccounts => ', walletAccounts);
+        connectAct(network, walletAccounts[0].address, WALLET_PERA);
         closeDialog();
       });
   };
 
   return (
-    <Dialog open={dialogOpened} onClose={() => closeDialog()} maxWidth="xs" fullWidth>
-      {/* Header */}
-      <Stack direction="row" justifyContent="space-between" alignItems="center" px={2} py={2}>
-        <Stack direction="row" alignItems="center" spacing={1}>
-          <MuiIcon component={Icon} icon="material-symbols:account-balance-wallet-outline" color="primary" fontSize="large" />
-          <Typography component="h2" variant="h5" fontWeight={700}>
-            Connect wallet
-          </Typography>
+    <>
+      <Dialog open={dialogOpened} onClose={() => closeDialog()} maxWidth="xs" fullWidth>
+        {/* Header */}
+        <Stack direction="row" justifyContent="space-between" alignItems="center" px={2} py={2}>
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <MuiIcon component={Icon} icon="material-symbols:account-balance-wallet-outline" color="primary" fontSize="large" />
+            <Typography component="h2" variant="h5" fontWeight={700}>
+              Connect wallet
+            </Typography>
+          </Stack>
+
+          <IconButton onClick={() => closeDialog()}>
+            <Icon icon="material-symbols:close-rounded" />
+          </IconButton>
         </Stack>
 
-        <IconButton onClick={() => closeDialog()}>
-          <Icon icon="material-symbols:close-rounded" />
-        </IconButton>
-      </Stack>
+        <DialogContent>
+          <List>
+            <ListItem>
+              <ListItemButton onClick={() => connectByAlgoSigner()}>
+                <ListItemAvatar>
+                  <Avatar src="/assets/images/algo-signer.jpg" alt="" />
+                </ListItemAvatar>
+                <ListItemText>AlgoSigner</ListItemText>
+                <ListItemIcon>
+                  <MuiIcon
+                    component={Icon}
+                    icon="material-symbols:arrow-forward-ios-rounded"
+                    fontSize="medium"
+                    sx={{ color: 'black' }}
+                  />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
 
-      <DialogContent>
-        <List>
-          <ListItem>
-            <ListItemButton onClick={() => connectByAlgoSigner()}>
-              <ListItemAvatar>
-                <Avatar src="/assets/images/algo-signer.jpg" alt="" />
-              </ListItemAvatar>
-              <ListItemText>AlgoSigner</ListItemText>
-              <ListItemIcon>
-                <MuiIcon
-                  component={Icon}
-                  icon="material-symbols:arrow-forward-ios-rounded"
-                  fontSize="medium"
-                  sx={{ color: 'black' }}
-                />
-              </ListItemIcon>
-            </ListItemButton>
-          </ListItem>
+            <ListItem>
+              <ListItemButton onClick={() => connectByMyAlgo()}>
+                <ListItemAvatar>
+                  <Avatar src="/assets/images/myalgo-wallet.svg" alt="" />
+                </ListItemAvatar>
+                <ListItemText>MyAlgo Wallet</ListItemText>
+                <ListItemIcon>
+                  <MuiIcon
+                    component={Icon}
+                    icon="material-symbols:arrow-forward-ios-rounded"
+                    fontSize="medium"
+                    sx={{ color: 'black' }}
+                  />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
 
-          <ListItem>
-            <ListItemButton onClick={() => connectByMyAlgo()}>
-              <ListItemAvatar>
-                <Avatar src="/assets/images/myalgo-wallet.svg" alt="" />
-              </ListItemAvatar>
-              <ListItemText>MyAlgo Wallet</ListItemText>
-              <ListItemIcon>
-                <MuiIcon
-                  component={Icon}
-                  icon="material-symbols:arrow-forward-ios-rounded"
-                  fontSize="medium"
-                  sx={{ color: 'black' }}
-                />
-              </ListItemIcon>
-            </ListItemButton>
-          </ListItem>
+            <ListItem>
+              <ListItemButton onClick={() => connectByPera()}>
+                <ListItemAvatar>
+                  <Avatar src="/assets/images/pera-wallet.svg" alt="" />
+                </ListItemAvatar>
+                <ListItemText>Pera Wallet</ListItemText>
+                <ListItemIcon>
+                  <MuiIcon
+                    component={Icon}
+                    icon="material-symbols:arrow-forward-ios-rounded"
+                    fontSize="medium"
+                    sx={{ color: 'black' }}
+                  />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </DialogContent>
 
-          <ListItem>
-            <ListItemButton onClick={() => connectByPera()}>
-              <ListItemAvatar>
-                <Avatar src="/assets/images/pera-wallet.svg" alt="" />
-              </ListItemAvatar>
-              <ListItemText>Pera Wallet</ListItemText>
-              <ListItemIcon>
-                <MuiIcon
-                  component={Icon}
-                  icon="material-symbols:arrow-forward-ios-rounded"
-                  fontSize="medium"
-                  sx={{ color: 'black' }}
-                />
-              </ListItemIcon>
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </DialogContent>
-
-      <Typography textAlign="center" mb={5} color={grey[600]} fontSize={14}>
-        By connecting, I accept Dahliadesk Terms of Service
-      </Typography>
-    </Dialog>
+        <Typography textAlign="center" mb={5} color={grey[600]} fontSize={14}>
+          By connecting, I accept Dahliadesk Terms of Service
+        </Typography>
+      </Dialog>
+      <DialogSelectWalletAccount
+        accounts={accounts}
+        dialogOpened={dialogSelectAccountOpened}
+        setDialogOpened={setDialogSelectAccountOpened}
+      />
+    </>
   );
 }
---
+++ src/components/DialogSelectWalletAccount.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Box, Dialog, DialogContent, DialogTitle, IconButton, ListItemText, MenuItem, MenuList, Stack, Typography } from '@mui/material';
+import { Icon } from '@iconify/react';
+import { blue } from '@mui/material/colors';
+import { WALLET_ALGO_SIGNER } from '../utils/constants';
+import useConnectWallet from '../hooks/useConnectWallet';
+
+export default function DialogSelectWalletAccount({ accounts, dialogOpened, setDialogOpened }) {
+  const { connectAct, network } = useConnectWallet();
+
+  const closeDialog = () => {
+    setDialogOpened(false);
+  };
+
+  const selectAccount = (account) => {
+    connectAct(network, account.address, WALLET_ALGO_SIGNER);
+  };
+
+  return (
+    <Dialog open={dialogOpened} onClose={() => closeDialog()} maxWidth="xs" fullWidth>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" pr={2} py={2}>
+        <DialogTitle fontWeight={700}>
+          Select Account
+        </DialogTitle>
+        <IconButton onClick={() => closeDialog()}>
+          <Icon icon="material-symbols:close-rounded" />
+        </IconButton>
+      </Stack>
+
+      <DialogContent>
+        <Stack spacing={2}>
+          {accounts.map(accountItem => (
+            <Box
+              key={accountItem.address}
+              sx={{ overflowWrap: 'anywhere', bgcolor: blue[100], p: 2, cursor: 'pointer' }}
+              onClick={() => selectAccount(accountItem)}
+            >
+              {accountItem.address}
+            </Box>
+          ))}
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+}
